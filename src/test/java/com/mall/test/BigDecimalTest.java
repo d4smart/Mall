47049fb83cd530b6645eb8c02983f5411c348920
闾晓冬 <d4smart@foxmail.com>
2017-05-20 20:34:00 +0800

    支付模块测试完成，大致通过（bug：支付功能二维码上传到ftp服务器有bug，较难解决）

--- src/test/java/com/mall/test/BigDecimalTest.java
+++ src/test/java/com/mall/test/BigDecimalTest.java
@@ -1,7 +1,13 @@
 package com.mall.test;
 
+import com.google.common.collect.Lists;
+import com.mall.util.FTPUtil;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 /**
@@ -24,5 +30,13 @@ public class BigDecimalTest {
         BigDecimal b1 = new BigDecimal("0.01");
         BigDecimal b2 = new BigDecimal("0.05");
         System.out.println(b1.add(b2));
+    }
+
+    @Test
+    public void test3() throws IOException {
+        // File file = new File("C:\\Users\\23037\\IdeaProjects\\mall\\target\\mall\\path", "qr-1491753014256.png");
+        File file = new File("C:/Users/23037/IdeaProjects/mall/target/mall/path", "qr-1491753014256.png");
+        // C:/Users/23037/IdeaProjects/mall/target/mall/path qr_1491753014256.png
+        FTPUtil.uploadFile(Lists.newArrayList(file));
     }
 }
